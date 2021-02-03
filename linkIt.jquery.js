d9d82446cb83bc4489a2cd0b5aa1de2fca1873c2
AdilHussain167 <karimbaloch163@gmail.com>
2021-02-03 00:09:43 -0800

    first commit s

---
+++ linkIt.jquery.js
@@ -0,0 +1,7 @@
+echo "# jquery.linkIt" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:AdilHussain167/jquery.linkIt.git
+git push -u origin main
